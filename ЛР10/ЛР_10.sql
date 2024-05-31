2f9772f41d791154de2ee39132d1922152b89e6f
BoikoBogdann <116552631+BoikoBogdann@users.noreply.github.com>
2024-05-31 18:11:31 +0300

    lab_10

---
+++ ЛР10/ЛР_10.sql
@@ -0,0 +1,48 @@
+﻿-- ЗАДАЧА 1
+SELECT DISTINCT MFR
+FROM ORDERS
+WHERE YEAR(ORDER_DATE) = 2008
+ORDER BY MFR;
+
+-- ЗАДАЧА 2
+SELECT 
+    DATEDIFF(YEAR, HIRE_DATE, GETDATE()) AS YearsWorked,
+    COUNT(*) AS EmployeeCount
+FROM SALESREPS
+GROUP BY DATEDIFF(YEAR, HIRE_DATE, GETDATE())
+ORDER BY EmployeeCount DESC;
+
+
+-- ЗАДАЧА 3
+WITH HireCounts AS (
+    SELECT 
+        YEAR(HIRE_DATE) AS HireYear, 
+        MONTH(HIRE_DATE) AS HireMonth, 
+        COUNT(*) AS EmployeeCount
+    FROM SALESREPS
+    GROUP BY YEAR(HIRE_DATE), MONTH(HIRE_DATE)
+)
+SELECT 
+    HireYear, 
+    HireMonth, 
+    EmployeeCount
+FROM HireCounts
+WHERE EmployeeCount = (
+    SELECT MAX(EmployeeCount) 
+    FROM HireCounts
+)
+ORDER BY HireYear, HireMonth;
+
+-- ЗАДАЧА 4
+SELECT 
+    DATEPART(WEEKDAY, ORDER_DATE) AS DayOfWeekNumber,
+    DATENAME(WEEKDAY, ORDER_DATE) AS DayOfWeekName,
+    COUNT(DISTINCT ORDER_NUM) AS TotalOrders,
+    SUM(AMOUNT) AS TotalSales,
+    SUM(QTY) AS TotalQuantitySold
+FROM ORDERS
+WHERE MONTH(ORDER_DATE) IN (12, 1, 2)
+GROUP BY DATEPART(WEEKDAY, ORDER_DATE), DATENAME(WEEKDAY, ORDER_DATE)
+ORDER BY DayOfWeekNumber;
+
+
